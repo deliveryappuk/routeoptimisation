9adb6042738b9066c6802c9f722a51a0ec909b81
Hossein Saneipour <h.saneipour@gmail.com>
2024-05-12 22:33:07 +0100

    update def

--- routingpr/optimize_routes.py
+++ routingpr/optimize_routes.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 
 class OptimizeRoutes:
+    
     def get_json_data(self):
         static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
         excel_file_path = os.path.join(static_dir, 'locations.xlsx')
@@ -11,13 +12,28 @@ class OptimizeRoutes:
 
         jsonData = {"requests": []}
         df = pd.read_excel(excel_file_path, sheet_name='locations')
+
+        column_names = df.columns.tolist()
+
         for index, row in df.iterrows():
-            jsonData["requests"].append({
-                "Order ID": int(row["OrderID"]),
-                "Collection Postcode": row["CollectionPostcode"],
-                "Depot": row["Depot"],
-                "Collection From (HH:MM)": str(row["CollectionFrom"]),  
-                "Collection To (HH:MM)": str(row["CollectionTo"]),     
-                "Stop Postcode": row["StopPostcode"]
-            })
+            request_data = {}
+            for column in column_names:
+                request_data[column] = row[column]
+            jsonData["requests"].append(request_data)
+
         return jsonData
+    
+    def optimize_stops(self, json_data):
+        optimized_data = {"routes": []}
+        for request in json_data["requests"]:
+            added = False
+            for route in optimized_data["routes"]:
+                if route[-1]["CollectionFrom"] <= request["CollectionTo"]:
+                    route.append(request)
+                    added = True
+                    break
+            if not added:
+                optimized_data["routes"].append([request])
+
+        return optimized_data
+    
--- routingpr/views.py
+++ routingpr/views.py
@@ -3,7 +3,7 @@ from .optimize_routes import OptimizeRoutes
 def my_view(request):
     optimize_routes = OptimizeRoutes()
     json_data = optimize_routes.get_json_data()
-    print(json_data)
-    return render(request, 'request.html', {'jsonData': json_data})
+    json_optimized=optimize_routes.optimize_stops(json_data)
+    return render(request, 'request.html', {'jsonData': json_data,'jsonResponse': json_optimized})
 def index(request):
     return render(request, 'index.html')
